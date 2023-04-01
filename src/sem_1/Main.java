112297f957829d3fa9927220467b15c885ba61bf
Wilovod <wilovod@gmail.com>
2023-04-01 20:19:40 +0700

    add sem_2

--- src/sem_1/Main.java
+++ src/sem_1/Main.java
@@ -7,7 +7,7 @@ import sem_1.task.Task_3;
 public class Main {
 
     public static void main(String[] args) {
-//        Task_1 task_1 = new Task_1();
+//        Task_1 task = new Task_1();
 //        Task_1.hello();
 //        Task_2.hello();
         int[] array = new int[] {1,1,1,0,1,1,1,1};
---
+++ src/sem_2/Main.java
@@ -0,0 +1,21 @@
+package sem_2;
+
+
+import sem_2.task.Task_1;
+import sem_2.task.Task_2;
+import sem_2.task.Task_3;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(Task_1.getRepeatString(6));
+//        System.out.println(Task_2.stringCompression("aaaabbbcdd"));
+        Task_3.Write("TEST");
+    }
+
+
+}
+
+
